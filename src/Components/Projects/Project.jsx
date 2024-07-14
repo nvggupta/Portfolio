cf303a276136b94092ca91ba0c1b55591fef0540
nvggupta <ng969028@gmail.com>
2024-07-14 22:02:20 +0530

    error resolve

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
-import Projects from "./Project.json";
+
 import "../Home/Header.css";
 import Card from './Card';
 
 function Project() {
+    
+[
+    {
+        "image": "Assets/download (1).png",
+        "Name": "Paragraph Generator",
+        "link":"https://paragenerator-ten.vercel.app/"
+    },
+    {
+        
+        "image": "Assets/download (2).png",
+        "Name": "Family Wellness",
+        "link":"https://vercel.com/nikhil-gupta/family-wellness"
+    },
+    {
+        
+        
+            "image": "Assets/download.png",
+            "Name": "Education Planner",
+            "link":"https://educator-planner.vercel.app/"
+        
+    }
+]
   return (
     <>
       <div className='flex flex-wrap gap-6 section1 items-start justify-center mt-3'>
